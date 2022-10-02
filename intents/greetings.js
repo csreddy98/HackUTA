33013e9ba92a62b48d1a791c57ff70658dbf31da
Chandu <csreddy1998@gmail.com>
2022-10-02 11:28:35 -0500

    Message Updated

--- intents/greetings.js
+++ intents/greetings.js
@@ -2,14 +2,14 @@ const greeting = (entity) => {
     return `
 Hello, how can I help you? 
 You can ask me about the following:
-1. Policy Information
-2. Claim Information
-3. Payment Information
-4. Contact Information
-5. Personal Information
-6. Raise a Complaint or Get status of a complaint
-7. Find nearest hospitals/servicing centers in your area under our network
-8. Get Contact Information of our Customer Care
+- Policy Information
+- Claim Information
+- Payment Information
+- Contact Information
+- Personal Information
+- Raise a Complaint or Get status of a complaint
+- Find nearest hospitals/servicing centers in your area under our network
+- Get Contact Information of our Customer Care
     `;
 };
 
